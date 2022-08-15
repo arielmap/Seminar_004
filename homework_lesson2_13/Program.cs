8f30d063d062d354843d49a0f7c4e801bd262612
Vladislav izmailov <arielmap@icloud.com>
2022-08-15 19:46:28 +0300

    homework13

---
+++ homework_lesson2_13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача номер 13
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > -100 && number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    if (number < 0)
+    {
+        number = -1 * number;
+    }
+    string thirdnumber = Convert.ToString(number);
+    Console.WriteLine(thirdnumber[2]);
+}
