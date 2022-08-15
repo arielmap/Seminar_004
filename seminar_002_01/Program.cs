5e4079ac39dc0e0900327013bd2154fb72852cb8
Vladislav izmailov <arielmap@icloud.com>
2022-08-15 14:53:47 +0300

    1 program

--- seminar_002_01/Program.cs
+++ seminar_002_01/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int number1 = new Random().Next(1, 10);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
+int result = number1 + number2;
+Console.WriteLine(result); 
