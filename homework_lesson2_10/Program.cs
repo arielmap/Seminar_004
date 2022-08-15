ca38853ff13fc3764b5dd0ca30f6fd59de7f62c9
Vladislav izmailov <arielmap@icloud.com>
2022-08-15 19:33:13 +0300

    Homework10

---
+++ homework_lesson2_10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача номер 10
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 0)
+{
+    number = -1 * number % 100 / 10;
+}
+else
+{
+    number = number % 100 / 10;
+}
+Console.WriteLine(number);
