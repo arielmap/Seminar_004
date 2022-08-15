9f0954c8c14c24519592a135a7b3db7ec040242b
Vladislav izmailov <arielmap@icloud.com>
2022-08-15 20:25:55 +0300

    homework15

---
+++ homework_lesson2_15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели ");
+Console.WriteLine("1 - Понедельник, 2 - Вторник, 3 - Среда, 4 - Чтетверг, 5 - Пятница, 6 - Суббота, 7 - Воскресенье");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 == 6 || number1 == 7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
